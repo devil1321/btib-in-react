9bdbcd050c7d8d8fb3c7372e3d6503822082476f
devil1321 <s.dominik1321@gmail.com>
2021-05-13 22:47:45 +0200

    btib navbar sm

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,52 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 const Header = () => {
+    const menuToggle = (e) =>{
+        const burger = document.querySelector('.burger')
+        const list = document.querySelector('.header__list')
+        burger.classList.toggle('open')
+        if(burger.classList.contains('open')){
+            list.style.height = '290px'
+            list.style.borderTop = '1px solid rgba(250, 250, 250, 0.12)';
+        }else{
+            list.style.height = '0px'
+            list.style.borderTop = '0px';
+        }
+    }
     return (
         <div className="header">
             <img src='/assets/logo.png' alt="logo"/>
+            <div className="header__menu-sm" onClick={()=>{menuToggle()}}>
+                <p>MENU</p>
+                <div className="burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <ul className="header__list">
+                <Link to="/">
+                    <li className="header__item">STRONA GŁÓWNA</li>
+                </Link>
+                <Link to="/o-firmie">
+                    <li className="header__item">O FIRMIE</li>
+                </Link>
+                <Link to="/oferta">
+                    <li className="header__item">OFERTA</li>
+                </Link>
+                <Link to="/realizacje">
+                    <li className="header__item">REALIZACJE</li>
+                </Link>
+                <Link to="/akty-prawne">
+                    <li className="header__item">AKTY PRAWNE</li>
+                </Link>
+                <Link to="/wspolpraca">
+                    <li className="header__item">WSPÓŁPRACA</li>
+                </Link>
+                <Link to="kontakt">
+                    <li className="header__item">KONTAKT</li>
+                </Link>
+            </ul>
             <div className="header__widget-wrapper">
                 <div className="header__widget">
                     <i className="fa  fa-phone  fa-3x"></i>
