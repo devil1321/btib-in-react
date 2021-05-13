b72fc07fad67c3c4291f617237e301afab03cdee
devil1321 <s.dominik1321@gmail.com>
2021-05-13 15:52:32 +0200

    btib icons fa

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import 'font-awesome/css/font-awesome.min.css'; 
-import './styles/themes/default/theme.scss'
+
 
 ReactDOM.render(
   <React.StrictMode>
